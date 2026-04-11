5f5b759844a16aea1c94506f3a83312fa1be0195
Kurtisc9 <89293278+Kurtisc9@users.noreply.github.com>
2026-04-11 16:43:14 -0400

    Delete project/electron/roject/src/lib directory

--- project/electron/roject/src/lib/getBridge.ts
+++
@@ -1,7 +0,0 @@
-export function getBridge() {
-  if (!window.team9Bridge) {
-    throw new Error('team9Bridge is unavailable. Preload may not be connected.');
-  }
-
-  return window.team9Bridge;
-}
