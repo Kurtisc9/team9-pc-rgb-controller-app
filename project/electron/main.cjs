e74ceb7475af49f8514599553367a2b54e2e92cb
Kurtisc9 <89293278+Kurtisc9@users.noreply.github.com>
2026-04-11 16:10:29 -0400

    Add main Electron application file

---
+++ project/electron/main.cjs
@@ -0,0 +1,56 @@
+const path = require('path');
+const { app, BrowserWindow, ipcMain, shell, dialog } = require('electron');
+const { registerAppHandlers } = require('./ipc/app.handlers.cjs');
+const { registerLibraryHandlers } = require('./ipc/library.handlers.cjs');
+const { registerDisplayHandlers } = require('./ipc/display.handlers.cjs');
+const { registerDiagnosticsHandlers } = require('./ipc/diagnostics.handlers.cjs');
+
+const isDev = !app.isPackaged;
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1600,
+    height: 980,
+    minWidth: 1200,
+    minHeight: 760,
+    backgroundColor: '#0b0f14',
+    autoHideMenuBar: true,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.cjs'),
+      contextIsolation: true,
+      nodeIntegration: false,
+      sandbox: false,
+    },
+  });
+
+  if (isDev) {
+    win.loadURL('http://localhost:5173');
+    win.webContents.openDevTools({ mode: 'detach' });
+  } else {
+    win.loadFile(path.join(__dirname, '../dist/index.html'));
+  }
+
+  win.webContents.setWindowOpenHandler(({ url }) => {
+    shell.openExternal(url);
+    return { action: 'deny' };
+  });
+
+  return win;
+}
+
+app.whenReady().then(() => {
+  const mainWindow = createWindow();
+
+  registerAppHandlers({ app });
+  registerLibraryHandlers({ dialog });
+  registerDisplayHandlers();
+  registerDiagnosticsHandlers({ app });
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
